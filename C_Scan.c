7001a4b5237c365aaf89932783fbd8509d3b2eb8
Vis_maya_ <vismayavinod250@gmail.com>
2025-07-08 21:51:42 +0530

    C_Scan.c

---
+++ C_Scan.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+int head, a[20], i, distance, n, seektime, size;
+
+void bubbleSort(int arr[], int n) {
+    int temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    printf("\nEnter Head position: ");
+    scanf("%d", &head);
+
+    printf("\nEnter number of disk requests: ");
+    scanf("%d", &n);
+
+    printf("\nEnter the disk size: ");
+    scanf("%d", &size);
+
+    printf("\nEnter the disk requests: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    a[n] = head; // include head in the request array
+    n++;
+
+    bubbleSort(a, n);
+
+    int pos;
+    for (i = 0; i < n; i++) {
+        if (a[i] == head) {
+            pos = i;
+            break;
+        }
+    }
+
+    seektime = 0;
+
+    printf("\n\tC-SCAN DISK SCHEDULING\n\n");
+
+    // Move from head to end
+    for (i = pos; i < n - 1; i++) {
+        distance = a[i + 1] - a[i];
+        printf("Head movement from %d to %d : %d\n", a[i], a[i + 1], distance);
+        seektime += distance;
+    }
+
+    // Move from last request to end of disk
+    if (a[n - 1] != size - 1) {
+        distance = (size - 1) - a[n - 1];
+        printf("Head movement from %d to %d : %d\n", a[n - 1], size - 1, distance);
+        seektime += distance;
+    }
+
+    // Move from end of disk to 0
+    distance = size - 1;
+    printf("Head movement from %d to 0 : %d\n", size - 1, distance);
+    seektime += distance;
+
+    // Move from 0 to beginning of queue
+    for (i = 0; i < pos; i++) {
+        if (i == 0) {
+            distance = a[i];
+        } else {
+            distance = a[i] - a[i - 1];
+        }
+
+        printf("Head movement from %d to %d : %d\n", i == 0 ? 0 : a[i - 1], a[i], distance);
+        seektime += distance;
+    }
+
+    printf("\nTotal seek time is : %d\n", seektime);
+
+    return 0;
+}
